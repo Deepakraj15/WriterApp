2c5c75337e99fd5c543437bf8eaa8c5679a9f4c8
Deepakraj15 <deepakraj15112002@gmail.com>
2022-09-22 10:57:35 +0530

    initial commit

---
+++ client/source/script/login.js
@@ -0,0 +1,12 @@
+
+function inputSelect() {
+    document.getElementById("user-text").style.display = "block";
+    document.getElementById("password-text").style.display = "block";
+    document.getElementById("username").placeholder = "";
+    document.getElementById("password").placeholder = "";
+}
+function formValidation() {
+    var uname = document.getElementById("user-text");
+    var password = document.getElementById("password-text");
+
+}
