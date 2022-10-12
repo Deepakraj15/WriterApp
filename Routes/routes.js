5219d4f5c667faf5aef6bc2a502c36e2b62f27d3
Deepakraj15 <deepakraj15112002@gmail.com>
2022-10-12 14:09:29 +0530

    signup page added

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import HomePage from '../controllers/HomePage.js';
 import LoginPage from '../controllers/LoginPage.js';
+import SignUp from '../controllers/SignUp.js';
 const router = express.Router();
 
 router.get('/', HomePage);
 router.get('/login', LoginPage);
+router.get('/signup', SignUp)
 //router.post('/loginVerfications', LoginVerfication);
 export default router;
