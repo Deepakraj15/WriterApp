7c15ff04e5742ad67893292d1a98852d4e51afb2
Deepakraj15 <deepakraj15112002@gmail.com>
2022-10-28 09:26:27 +0530

    schema added

--- models/CreateUser.js
+++ models/CreateUser.js
@@ -5,6 +5,18 @@ const CreateUser = new mongoose.Schema(
         name: {
             type: 'string',
             required: true,
+        },
+        email: {
+            type: 'email',
+            required: true,
+        },
+        password: {
+            type: 'string',
+            required: true,
+        },
+        age: {
+            type: 'integer',
+            required: true,
         }
     }
 )
